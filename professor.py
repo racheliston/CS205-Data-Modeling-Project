09c7d270dc4a1776db65c966a83a95a23bc75aed
rliston <rliston@uvm.edu>
2021-10-18 21:14:50 -0400

    Added comments to draw connections between classes

---
+++ professor.py
@@ -0,0 +1,4 @@
+# professor provides the association between a student and a college
+# when a student joins a college within the university, an instance of Professor
+# is created -- shows that a college has a student 
+
