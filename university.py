9c7ca47056e9dd3c7fe36ae23abdaf64ff1192ce
rliston <rliston@uvm.edu>
2021-10-18 21:11:27 -0400

    Added comments to draw connections between classes

---
+++ university.py
@@ -0,0 +1,10 @@
+import professor
+
+# design patter -- singleton pattern
+# define a static instance of the class itself and return
+# that instance in a getter
+#
+# when a college gets a new student, an instance of Professor is created
+# the association between the college and the student is shown by
+# adding the student to the college's list of students
+
