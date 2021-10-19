a84f4111f3cae8e309596150dbfd9e579c403759
rliston <rliston@uvm.edu>
2021-10-19 14:00:22 -0400

    Fixed university class relationships

--- university.py
+++ university.py
@@ -1,10 +1,10 @@
-import professor
+import college
 
 # design patter -- singleton pattern
 # define a static instance of the class itself and return
 # that instance in a getter
 #
-# when a college gets a new student, an instance of Professor is created
-# the association between the college and the student is shown by
-# adding the student to the college's list of students
+# when a professor gets a new student, an instance of College is created
+# the association between the professor and the student is shown by
+# adding the student to the professor's list of students
 
